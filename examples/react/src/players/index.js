c17d72b6009a1f2e7df92e88a2b2299426bbf447
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-04-30 23:05:53 +0430

    chore: prepare for release

--- examples/react/src/players/index.js
+++
@@ -1,5 +0,0 @@
-export const OnlyPlayPauseButton = require('./OnlyPlayPauseButton').default
-export const NoPreload = require('./NoPreload').default
-export const SwapSource = require('./SwapSource').default
-export const AutoPlay = require('./AutoPlay').default
-export const FullControl = require('./FullControl').default
